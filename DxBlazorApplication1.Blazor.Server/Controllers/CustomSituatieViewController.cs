18fa6ed1b9839bd6fc3074a256c4fcd435a66ed2
Bogdan <bogdanp647@gmail.com>
2025-02-10 10:41:40 +0200

    Refactor, deleted CustomSituatieViewController

--- DxBlazorApplication1.Blazor.Server/Controllers/CustomSituatieViewController.cs
+++ DxBlazorApplication1.Blazor.Server/Controllers/CustomSituatieViewController.cs
@@ -31,8 +31,8 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
 {
     public partial class CustomSituatieViewController : ViewController<DetailView>
     {
-        SimpleAction refreshSituatie; 
-        SimpleAction listeazaSituatie; 
+        SimpleAction refreshSituatie;
+        SimpleAction listeazaSituatie;
         DxBlazorApplication1EFCoreDbContext dbContext;
         private IReportDataSourceHelper reportDataSourceHelper;
 
@@ -43,7 +43,7 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
             refreshSituatie = new SimpleAction(this, "RefreshSituatieAction", "CustomSituatieActionsCategory")
             {
                 Caption = "Refresh",
-                ImageName = "Action_Refresh"            
+                ImageName = "Action_Refresh"
             };
             refreshSituatie.Execute += RefreshSituatie_Execute;
 
@@ -53,7 +53,84 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
                 ImageName = "BO_List"
             };
             listeazaSituatie.Execute += ListeazaSituatie_Execute;
+        }
 
+        private BindingList<ItemSituatieNonPersistent> GetSituatieItems()
+        {
+            CustomSituatieNonPersistent parametriCurenti = View.CurrentObject as CustomSituatieNonPersistent;
+
+            if (parametriCurenti.DataInceput == DateTime.MinValue || parametriCurenti.DataSfarsit == DateTime.MinValue)
+            {
+                Application.ShowViewStrategy.ShowMessage("Campurile 'Data Inceput' si 'Data Sfarsit' sunt obligatorii.", InformationType.Error, 3000);
+                return null;
+            }
+            if (parametriCurenti.DataInceput > parametriCurenti.DataSfarsit)
+            {
+                Application.ShowViewStrategy.ShowMessage("Data Inceput nu poate fi mai mare decat Data Sfarsit.", InformationType.Error, 3000);
+                return null;
+            }
+
+            BindingList<ItemSituatieNonPersistent> itemsList = new BindingList<ItemSituatieNonPersistent>();
+
+            if (parametriCurenti.ToateGestiunile)
+            {
+                parametriCurenti.Gestiune = null;
+            }
+            else if (parametriCurenti.OGestiune)
+            {
+                if (parametriCurenti.Gestiune == null)
+                {
+                    Application.ShowViewStrategy.ShowMessage("Va rugam selectati o gestiune.", InformationType.Error, 3000);
+                    return null;
+                }
+            }
+            else
+            {
+                Application.ShowViewStrategy.ShowMessage("Va rugam selectati o gestiune.", InformationType.Error, 3000);
+                return null;
+            }
+
+            var storedProcedure = parametriCurenti.TipAfisare == TipAfisare.Intrari ? "CalculIntrari" : "CalculIesiri";
+
+            using var command = dbContext.Database.GetDbConnection().CreateCommand();
+            command.CommandText = storedProcedure;
+            command.CommandType = CommandType.StoredProcedure;
+
+            var dataInceputParam = new SqlParameter("DataInceput", parametriCurenti.DataInceput);
+            command.Parameters.Add(dataInceputParam);
+
+            var dataSfarsitParam = new SqlParameter("DataSfarsit", parametriCurenti.DataSfarsit);
+            command.Parameters.Add(dataSfarsitParam);
+
+            if (parametriCurenti.Gestiune != null)
+            {
+                var gestiuneParam = new SqlParameter("GestiuneID", parametriCurenti.Gestiune.ID);
+                command.Parameters.Add(gestiuneParam);
+            }
+
+            using var adapter = new SqlDataAdapter(command as SqlCommand);
+            var dataSet = new DataSet();
+            adapter.Fill(dataSet);
+
+            DataTable dataTableProduseIntrari = dataSet.Tables[0];
+            foreach (DataRow row in dataTableProduseIntrari.Rows)
+            {
+                ItemSituatieNonPersistent itemSituatie = new ItemSituatieNonPersistent
+                {
+                    ID = Guid.NewGuid(),
+                    NumarMiscare = row["NumarMiscare"] != DBNull.Value ? Convert.ToInt32(row["NumarMiscare"]) : 0,
+                    DataMiscare = row["DataMiscare"] != DBNull.Value ? Convert.ToDateTime(row["DataMiscare"]) : DateTime.MinValue,
+                    DenumireProdus = row["DenumireProdus"] != DBNull.Value ? Convert.ToString(row["DenumireProdus"]) : "",
+                    Cantitate = row["Cantitate"] != DBNull.Value ? Convert.ToInt32(row["Cantitate"]) : 0,
+                    Valoare = row["Valoare"] != DBNull.Value ? Convert.ToInt32(row["Valoare"]) : 0,
+                    Total = row["Total"] != DBNull.Value ? Convert.ToInt32(row["Total"]) : 0,
+                    DenumireGestiune = row["DenumireGestiune"] != DBNull.Value ? Convert.ToString(row["DenumireGestiune"]) : "",
+                };
+
+                itemsList.Add(itemSituatie);
+            }
+
+            return itemsList;
         }
 
         private void ListeazaSituatie_Execute(object sender, SimpleActionExecuteEventArgs e)
@@ -67,77 +144,9 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
             // iau obiectul curent pentru a trimite mai departe ca parametri in procedura valorile introduse 
             CustomSituatieNonPersistent parametriCurenti = View.CurrentObject as CustomSituatieNonPersistent;
 
-            if (parametriCurenti.DataInceput == DateTime.MinValue || parametriCurenti.DataSfarsit == DateTime.MinValue)
-            {
-                Application.ShowViewStrategy.ShowMessage("Campurile 'Data Inceput' si 'Data Sfarsit' sunt obligatorii.", InformationType.Error, 3000);
+            var itemsList = GetSituatieItems();
+            if (itemsList == null)
                 return;
-            }
-            if (parametriCurenti.DataInceput > parametriCurenti.DataSfarsit)
-            {
-                Application.ShowViewStrategy.ShowMessage("Data Inceput nu poate fi mai mare decat Data Sfarsit.", InformationType.Error, 3000);
-                return;
-            }
-
-
-            BindingList<ItemSituatieNonPersistent> itemsList = new BindingList<ItemSituatieNonPersistent>();
-
-            // populez din procedura lista mea curenta
-
-            if (parametriCurenti.ToateGestiunile)
-            {
-                parametriCurenti.Gestiune = null;
-            }
-            else if (parametriCurenti.OGestiune)
-            {
-                if (parametriCurenti.Gestiune == null)
-                {
-                    Application.ShowViewStrategy.ShowMessage("Va rugam selectati o gestiune.", InformationType.Error, 3000);
-                    return;
-                }
-            }
-            var storedProcedure = parametriCurenti.TipAfisare == TipAfisare.Intrari ? "CalculIntrari" : "CalculIesiri";
-
-
-            using var command = dbContext.Database.GetDbConnection().CreateCommand();
-            command.CommandText = storedProcedure;
-            command.CommandType = CommandType.StoredProcedure;
-
-            var dataInceputParam = new SqlParameter("DataInceput", parametriCurenti.DataInceput);
-            command.Parameters.Add(dataInceputParam);
-
-            var dataSfarsitParam = new SqlParameter("DataSfarsit", parametriCurenti.DataSfarsit);
-            command.Parameters.Add(dataSfarsitParam);
-
-            if (parametriCurenti.Gestiune != null)
-            {
-                var gestiuneParam = new SqlParameter("GestiuneID", parametriCurenti.Gestiune.ID);
-                command.Parameters.Add(gestiuneParam);
-
-            }
-
-            using var adapter = new SqlDataAdapter(command as SqlCommand);
-            var dataSet = new DataSet();
-            adapter.Fill(dataSet);
-
-            DataTable dataTableProduseIntrari = dataSet.Tables[0];
-            foreach (DataRow row in dataTableProduseIntrari.Rows)
-            {
-                ItemSituatieNonPersistent itemSituatie = new ItemSituatieNonPersistent
-                {
-                    ID = Guid.NewGuid(),
-                    NumarMiscare = row["NumarMiscare"] != DBNull.Value ? Convert.ToInt32(row["NumarMiscare"]) : 0,
-                    DataMiscare = row["DataMiscare"] != DBNull.Value ? Convert.ToDateTime(row["DataMiscare"]) : DateTime.MinValue,
-                    DenumireProdus = row["DenumireProdus"] != DBNull.Value ? Convert.ToString(row["DenumireProdus"]) : "",
-                    Cantitate = row["Cantitate"] != DBNull.Value ? Convert.ToInt32(row["Cantitate"]) : 0,
-                    Valoare = row["Valoare"] != DBNull.Value ? Convert.ToInt32(row["Valoare"]) : 0,
-                    Total = row["Total"] != DBNull.Value ? Convert.ToInt32(row["Total"]) : 0,
-                    DenumireGestiune = row["DenumireGestiune"] != DBNull.Value ? Convert.ToString(row["DenumireGestiune"]) : "",
-                };
-
-                itemsList.Add(itemSituatie);
-            }
-
-
 
             IReportDataV2 reportData = ObjectSpace.FirstOrDefault<ReportDataV2>(data => data.DisplayName == "Situatie");
             string handler = ReportDataProvider.GetReportStorage(Frame.Application.ServiceProvider).GetReportContainerHandle(reportData);
@@ -146,9 +155,9 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
             ReportsModuleV2 reportsModule = ReportsModuleV2.FindReportsModule(Frame.Application.Modules);
             reportDataSourceHelper = reportsModule.ReportsDataSourceHelper;
 
-            reportDataSourceHelper.BeforeShowPreview += (s, e) => {
-                XtraReport report = e.Report as XtraReport;
-               
+            reportDataSourceHelper.BeforeShowPreview += (s, eArgs) => {
+                XtraReport report = eArgs.Report as XtraReport;
+
                 switch (report.GetType().Name)
                 {
                     case nameof(SituatieReport):
@@ -160,30 +169,25 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
                             rpo.DataInceput = parametriCurenti.DataInceput;
                             if (parametriCurenti.TipAfisare == TipAfisare.Intrari)
                                 rpo.TipSituatie = "intrari";
-                            else rpo.TipSituatie = "iesiri";
+                            else
+                                rpo.TipSituatie = "iesiri";
                             reportDataSourceHelper.SetXafReportParametersObject(report, rpo);
                         }
                         report.Parameters["DataInceput"].Value = parametriCurenti.DataInceput;
                         report.Parameters["DataSfarsit"].Value = parametriCurenti.DataSfarsit;
                         if (parametriCurenti.TipAfisare == TipAfisare.Intrari)
                             report.Parameters["TipSituatie"].Value = "intrari";
-                        else report.Parameters["TipSituatie"].Value = "iesiri";
+                        else
+                            report.Parameters["TipSituatie"].Value = "iesiri";
                         report.DataSource = itemsList;
                         break;
                 }
             };
 
-
             if (controller != null)
             {
                 controller.ShowPreview(handler, null, null);
             }
-
-        }
-
-        private void ReportDataSourceHelper_BeforeShowPreview(object sender, BeforeShowPreviewEventArgs e)
-        {
-           
         }
 
         private void RefreshSituatie_Execute(object sender, SimpleActionExecuteEventArgs e)
@@ -197,85 +201,9 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
             // iau obiectul curent pentru a trimite mai departe ca parametri in procedura valorile introduse 
             CustomSituatieNonPersistent parametriCurenti = View.CurrentObject as CustomSituatieNonPersistent;
 
-            if (parametriCurenti.DataInceput == DateTime.MinValue || parametriCurenti.DataSfarsit == DateTime.MinValue)
-            {
-                Application.ShowViewStrategy.ShowMessage("Campurile 'Data Inceput' si 'Data Sfarsit' sunt obligatorii.", InformationType.Error, 3000);
+            var itemsList = GetSituatieItems();
+            if (itemsList == null)
                 return;
-            }
-            if (parametriCurenti.DataInceput > parametriCurenti.DataSfarsit)
-            {
-                Application.ShowViewStrategy.ShowMessage("Data Inceput nu poate fi mai mare decat Data Sfarsit.", InformationType.Error, 3000);
-                return;
-            }
-
-
-            BindingList<ItemSituatieNonPersistent> itemsList = new BindingList<ItemSituatieNonPersistent>();
-
-            // populez din procedura lista mea curenta
-
-            
-            if (parametriCurenti.ToateGestiunile)
-            {
-                parametriCurenti.Gestiune = null;
-            }
-            else if (parametriCurenti.OGestiune)
-            {
-                if (parametriCurenti.Gestiune == null)
-                {
-                    Application.ShowViewStrategy.ShowMessage("Va rugam selectati o gestiune.", InformationType.Error, 3000);
-                    return;
-                }
-            }
-            else
-            {
-                Application.ShowViewStrategy.ShowMessage("Va rugam selectati o gestiune.", InformationType.Error, 3000);
-                return;
-            }
-
-
-
-            var storedProcedure = parametriCurenti.TipAfisare == TipAfisare.Intrari ? "CalculIntrari" : "CalculIesiri";
-
-
-            using var command = dbContext.Database.GetDbConnection().CreateCommand();
-            command.CommandText = storedProcedure;
-            command.CommandType = CommandType.StoredProcedure;
-
-            var dataInceputParam = new SqlParameter("DataInceput", parametriCurenti.DataInceput);
-            command.Parameters.Add(dataInceputParam);
-
-            var dataSfarsitParam = new SqlParameter("DataSfarsit", parametriCurenti.DataSfarsit);
-            command.Parameters.Add(dataSfarsitParam);
-
-            if (parametriCurenti.Gestiune != null)
-            {
-                var gestiuneParam = new SqlParameter("GestiuneID", parametriCurenti.Gestiune.ID);
-                command.Parameters.Add(gestiuneParam);
-
-            }
-
-            using var adapter = new SqlDataAdapter(command as SqlCommand);
-            var dataSet = new DataSet();
-            adapter.Fill(dataSet);
-
-            DataTable dataTableProduseIntrari = dataSet.Tables[0];
-            foreach (DataRow row in dataTableProduseIntrari.Rows)
-            {
-                ItemSituatieNonPersistent itemSituatie = new ItemSituatieNonPersistent
-                {
-                    ID = Guid.NewGuid(),
-                    NumarMiscare = row["NumarMiscare"] != DBNull.Value ? Convert.ToInt32(row["NumarMiscare"]) : 0,
-                    DataMiscare = row["DataMiscare"] != DBNull.Value ? Convert.ToDateTime(row["DataMiscare"]) : DateTime.MinValue,
-                    DenumireProdus = row["DenumireProdus"] != DBNull.Value ? Convert.ToString(row["DenumireProdus"]) : "",
-                    Cantitate = row["Cantitate"] != DBNull.Value ? Convert.ToInt32(row["Cantitate"]) : 0,
-                    Valoare = row["Valoare"] != DBNull.Value ? Convert.ToInt32(row["Valoare"]) : 0,
-                    Total = row["Total"] != DBNull.Value ? Convert.ToInt32(row["Total"]) : 0,
-                    DenumireGestiune = row["DenumireGestiune"] != DBNull.Value ? Convert.ToString(row["DenumireGestiune"]) : "",
-                };
-
-                itemsList.Add(itemSituatie);
-            }
-
 
             (listViewItemeSituatie as ListView).CollectionSource.List.Clear();
             foreach (var row in itemsList)
@@ -288,7 +216,6 @@ namespace DxBlazorApplication1.Blazor.Server.Controllers
             base.OnActivated();
             IObjectSpace objectSpace = (View.ObjectSpace as NonPersistentObjectSpace).AdditionalObjectSpaces[0];
             dbContext = ((DevExpress.ExpressApp.EFCore.EFCoreObjectSpace)objectSpace).DbContext as DxBlazorApplication1EFCoreDbContext;
-
         }
         protected override void OnViewControlsCreated()
         {
